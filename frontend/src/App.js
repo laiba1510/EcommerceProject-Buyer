3ea9bdf53e4d1f6e1f43e04f4ceb19759bd1a00a
laiba1510 <i200700@nu.edu.pk>
2023-07-29 10:28:20 +0500

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./Component/layout/header/NavBar";
 import "./App.css";
 import Footer from "./Component/layout/Footer/footer.jsx";
+import Home from "./Component/Home/Home";
 
 function App() {
   return (
-    <div>
-      {/* Your other components and content */}
-      <NavBar />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        {/* Your other components and content */}
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* Add more routes here if needed */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
 export default App;
-
--- frontend/src/Component/Home/Home.jsx
+++ frontend/src/Component/Home/Home.jsx
@@ -1,12 +1,21 @@
 import React , {Fragment  } from "react";
 
-
+import "./Home.css"
 const Home = () =>
 {
     return(
         <Fragment>
-            <div></div>
-            <div></div>
+           <div className="mainPage">
+            <p>WELCOME TO LAIBA AND LAIBA ECOM WEB</p>
+            <h1>all good in one place</h1>
+            <a href="#container">
+                <button>
+                    swipe up
+                </button>
+            </a>
+           </div>
         </Fragment>
     )
 }
+
+export default Home;
